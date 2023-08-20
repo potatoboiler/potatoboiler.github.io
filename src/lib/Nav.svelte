48944a57e7cc31be4bbf6ca7ec1a8e4b24f2c5d0
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-08-20 13:42:49 -0700

    Squashed commit of the following:
    commit 9750a889502e85179fcf82af2758ca23a7283de0
    Author: potatoboiler <61665896+potatoboiler@users.noreply.github.com>
    Date:   Sun Aug 20 13:41:11 2023 -0700
    
        Add about me
    
    commit 85a1433dfd85afed8e25a4b808be2cc30a793fad
    Author: potatoboiler <61665896+potatoboiler@users.noreply.github.com>
    Date:   Sun Aug 20 03:18:22 2023 -0700
    
        List of pages
    
    commit 8fb35ba1d75ae57cb712d068205d64ebb24b56bf
    Author: potatoboiler <61665896+potatoboiler@users.noreply.github.com>
    Date:   Sun Aug 20 02:56:12 2023 -0700
    
        Minimal page nav
    
    commit e2a8a073f146feac36fef6d94f423348d7f257d0
    Author: potatoboiler <61665896+potatoboiler@users.noreply.github.com>
    Date:   Sun Aug 20 02:21:16 2023 -0700
    
        Render md under posts
    
    commit a27fd1972b33f05b39c6c32d1ff5866b7c40992f
    Author: potatoboiler <61665896+potatoboiler@users.noreply.github.com>
    Date:   Sun Aug 20 01:10:23 2023 -0700
    
        Can render md as posts

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,6 +1,7 @@
 <div>
   <nav>
-    <a href="/"><h3 class="home">Home</h3></a>
-    <a href="/posts"><h3 class="posts">Posts</h3></a>
+    <a href="/"><h3 class="home">home</h3></a>
+    <a href="/about"><h3 class="about">about me</h3></a>
+    <a href="/posts"><h3 class="posts">posts</h3></a>
   </nav>
 </div>
