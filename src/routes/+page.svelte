9750a889502e85179fcf82af2758ca23a7283de0
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-08-20 13:41:11 -0700

    Add about me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>potato site</h1>
 <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    Welcome to my website! This site is meant to be a continual work in progress as I learn web dev.
 </p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<h1>About me</h1>
+
+<p>
+    Hiya! I'm a junior at UC Berkeley, currently studying computer science and
+    math. Interested in software work involving new and emerging technologies.
+    Favorite programming language is currently Rust.
+</p>
+
+<p>
+    You can find <a
+        href="https://drive.google.com/file/d/1XYE7VwTWtXaYUTagW3RcHTGX8Mp4K3Fz/view?usp=sharing"
+        >my resume here.</a
+    >
+</p>
+
+<h2>Favorite composers (in no particular order)</h2>
+<ul>
+    <li>Gustav Mahler</li>
+</ul>
