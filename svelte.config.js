a27fd1972b33f05b39c6c32d1ff5866b7c40992f
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-08-20 01:10:23 -0700

    Can render md as posts

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		mdsvex({ extensions: ['.md'] }),
+		vitePreprocess(),
+	],
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
 		})
-	}
+	},
+	prerender: { entries: ['*'] },
 };
