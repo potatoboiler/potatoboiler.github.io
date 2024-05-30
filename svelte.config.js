9a6e43abccb247ad263c3cc384601b36f97a83e9
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2024-05-29 18:23:28 -0700

    Update Svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { globSync } from 'glob';
 import { mdsvex } from 'mdsvex';
 
