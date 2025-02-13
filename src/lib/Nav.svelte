faa409d0e5ad569262e7f98068a5203f7b795e5b
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2025-02-13 13:58:35 -0800

    Add projects page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,5 +3,6 @@
     <a href="/"><h3 class="home">home</h3></a>
     <a href="/about"><h3 class="about">about me</h3></a>
     <a href="/posts"><h3 class="posts">posts</h3></a>
+    <a href="/projects"><h3 class="projects">projects</h3></a>
   </nav>
 </div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Posts</h1>
+<ul>
+    <li><a href="https://potatoboiler.github.io/aram-drafter">aram-drafter</a></li>
+</ul>
