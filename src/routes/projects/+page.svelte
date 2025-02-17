341b058e86b1d46f2c75da272aeef558377b0317
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2025-02-16 22:26:32 -0800

    Add bolero to projects list

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Projects</h1>
 <ul>
     <li><a href="https://potatoboiler.github.io/aram-drafter">aram-drafter</a></li>
+    <li><a href="https://potatoboiler.github.io/bolero">bolero</a></li>
 </ul>
